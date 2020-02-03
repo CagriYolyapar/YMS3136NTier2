44a33875bab6f2f3e99ee15e57ea2df2f7118656
tarik oksuz <tarikoksuz@hotmail.com>
2020-02-03 15:22:56 +0300

    Form Tasarımı Bitti. Sizlere Kolay Gelsin Arkadaşlar

--- Project.WINUI/Form1.Designer.cs
+++ Project.WINUI/Form1.Designer.cs
@@ -34,71 +34,95 @@
             this.lblKullaniciadi = new System.Windows.Forms.Label();
             this.lblsifre = new System.Windows.Forms.Label();
             this.btnekle = new System.Windows.Forms.Button();
+            this.btngec = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dtgrdkukkanici)).BeginInit();
             this.SuspendLayout();
             // 
             // dtgrdkukkanici
             // 
             this.dtgrdkukkanici.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgrdkukkanici.Location = new System.Drawing.Point(23, 12);
+            this.dtgrdkukkanici.Location = new System.Drawing.Point(28, 14);
+            this.dtgrdkukkanici.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtgrdkukkanici.Name = "dtgrdkukkanici";
-            this.dtgrdkukkanici.Size = new System.Drawing.Size(529, 150);
+            this.dtgrdkukkanici.Size = new System.Drawing.Size(607, 180);
             this.dtgrdkukkanici.TabIndex = 0;
             // 
             // txtkullaniciadi
             // 
-            this.txtkullaniciadi.Location = new System.Drawing.Point(180, 196);
+            this.txtkullaniciadi.Location = new System.Drawing.Point(220, 235);
+            this.txtkullaniciadi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtkullaniciadi.Multiline = true;
             this.txtkullaniciadi.Name = "txtkullaniciadi";
-            this.txtkullaniciadi.Size = new System.Drawing.Size(340, 26);
+            this.txtkullaniciadi.Size = new System.Drawing.Size(415, 31);
             this.txtkullaniciadi.TabIndex = 1;
             // 
             // txtkullanicisifresi
             // 
-            this.txtkullanicisifresi.Location = new System.Drawing.Point(180, 248);
+            this.txtkullanicisifresi.Location = new System.Drawing.Point(220, 298);
+            this.txtkullanicisifresi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtkullanicisifresi.Multiline = true;
             this.txtkullanicisifresi.Name = "txtkullanicisifresi";
-            this.txtkullanicisifresi.Size = new System.Drawing.Size(340, 26);
+            this.txtkullanicisifresi.Size = new System.Drawing.Size(415, 31);
             this.txtkullanicisifresi.TabIndex = 1;
             // 
             // lblKullaniciadi
             // 
             this.lblKullaniciadi.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.lblKullaniciadi.Location = new System.Drawing.Point(23, 196);
+            this.lblKullaniciadi.Location = new System.Drawing.Point(28, 235);
+            this.lblKullaniciadi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblKullaniciadi.Name = "lblKullaniciadi";
-            this.lblKullaniciadi.Size = new System.Drawing.Size(112, 26);
+            this.lblKullaniciadi.Size = new System.Drawing.Size(157, 31);
             this.lblKullaniciadi.TabIndex = 2;
-            this.lblKullaniciadi.Text = "Kullanıcı Adı";
+            this.lblKullaniciadi.Text = "KULLANICI ADI";
+            this.lblKullaniciadi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblsifre
             // 
             this.lblsifre.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.lblsifre.Location = new System.Drawing.Point(23, 248);
+            this.lblsifre.Location = new System.Drawing.Point(28, 298);
+            this.lblsifre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblsifre.Name = "lblsifre";
-            this.lblsifre.Size = new System.Drawing.Size(112, 26);
+            this.lblsifre.Size = new System.Drawing.Size(157, 31);
             this.lblsifre.TabIndex = 2;
-            this.lblsifre.Text = "Şifre";
+            this.lblsifre.Text = "ŞİFRE";
+            this.lblsifre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnekle
             // 
-            this.btnekle.Location = new System.Drawing.Point(273, 293);
+            this.btnekle.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnekle.Location = new System.Drawing.Point(334, 352);
+            this.btnekle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnekle.Name = "btnekle";
-            this.btnekle.Size = new System.Drawing.Size(108, 33);
+            this.btnekle.Size = new System.Drawing.Size(132, 40);
             this.btnekle.TabIndex = 3;
             this.btnekle.Text = "EKLE";
-            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.UseVisualStyleBackColor = false;
+            // 
+            // btngec
+            // 
+            this.btngec.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btngec.Location = new System.Drawing.Point(282, 400);
+            this.btngec.Margin = new System.Windows.Forms.Padding(4);
+            this.btngec.Name = "btngec";
+            this.btngec.Size = new System.Drawing.Size(234, 40);
+            this.btngec.TabIndex = 3;
+            this.btngec.Text = "DİĞER FORMA GEÇ";
+            this.btngec.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 413);
+            this.ClientSize = new System.Drawing.Size(667, 466);
+            this.Controls.Add(this.btngec);
             this.Controls.Add(this.btnekle);
             this.Controls.Add(this.lblsifre);
             this.Controls.Add(this.lblKullaniciadi);
             this.Controls.Add(this.txtkullanicisifresi);
             this.Controls.Add(this.txtkullaniciadi);
             this.Controls.Add(this.dtgrdkukkanici);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "Form1";
             this.Text = "Kullanıcılar";
             ((System.ComponentModel.ISupportInitialize)(this.dtgrdkukkanici)).EndInit();
@@ -115,6 +139,7 @@
         private System.Windows.Forms.Label lblKullaniciadi;
         private System.Windows.Forms.Label lblsifre;
         private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Button btngec;
     }
 }
 
---
+++ Project.WINUI/Form2.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Project.WINUI
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYokEt = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnSilinenler = new System.Windows.Forms.Button();
+            this.btnGuncellenenler = new System.Windows.Forms.Button();
+            this.btnAktifler = new System.Windows.Forms.Button();
+            this.txtAciklama = new System.Windows.Forms.TextBox();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.btnKategoriEkle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.dgvKategoriler = new System.Windows.Forms.DataGridView();
+            this.lblisim = new System.Windows.Forms.Label();
+            this.lblaciklama = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKategoriler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnYokEt
+            // 
+            this.btnYokEt.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnYokEt.Location = new System.Drawing.Point(435, 533);
+            this.btnYokEt.Name = "btnYokEt";
+            this.btnYokEt.Size = new System.Drawing.Size(197, 61);
+            this.btnYokEt.TabIndex = 13;
+            this.btnYokEt.Text = "YOK ET";
+            this.btnYokEt.UseVisualStyleBackColor = false;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnGuncelle.Location = new System.Drawing.Point(228, 533);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(197, 61);
+            this.btnGuncelle.TabIndex = 14;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnSil.Location = new System.Drawing.Point(14, 533);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(197, 61);
+            this.btnSil.TabIndex = 15;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = false;
+            // 
+            // btnSilinenler
+            // 
+            this.btnSilinenler.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnSilinenler.Location = new System.Drawing.Point(1202, 526);
+            this.btnSilinenler.Name = "btnSilinenler";
+            this.btnSilinenler.Size = new System.Drawing.Size(242, 68);
+            this.btnSilinenler.TabIndex = 10;
+            this.btnSilinenler.Text = "SİLİNENLERİ LİSTELE";
+            this.btnSilinenler.UseVisualStyleBackColor = false;
+            // 
+            // btnGuncellenenler
+            // 
+            this.btnGuncellenenler.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnGuncellenenler.Location = new System.Drawing.Point(1202, 442);
+            this.btnGuncellenenler.Name = "btnGuncellenenler";
+            this.btnGuncellenenler.Size = new System.Drawing.Size(242, 68);
+            this.btnGuncellenenler.TabIndex = 11;
+            this.btnGuncellenenler.Text = "GÜNCELLENENLERİ LİSTELE";
+            this.btnGuncellenenler.UseVisualStyleBackColor = false;
+            // 
+            // btnAktifler
+            // 
+            this.btnAktifler.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnAktifler.Location = new System.Drawing.Point(1202, 359);
+            this.btnAktifler.Name = "btnAktifler";
+            this.btnAktifler.Size = new System.Drawing.Size(242, 68);
+            this.btnAktifler.TabIndex = 12;
+            this.btnAktifler.Text = "AKTİFLERİ LİSTELE";
+            this.btnAktifler.UseVisualStyleBackColor = false;
+            // 
+            // txtAciklama
+            // 
+            this.txtAciklama.Location = new System.Drawing.Point(1202, 124);
+            this.txtAciklama.Name = "txtAciklama";
+            this.txtAciklama.Size = new System.Drawing.Size(242, 29);
+            this.txtAciklama.TabIndex = 8;
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(1202, 42);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(242, 29);
+            this.txtIsim.TabIndex = 9;
+            // 
+            // btnKategoriEkle
+            // 
+            this.btnKategoriEkle.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnKategoriEkle.Location = new System.Drawing.Point(1202, 184);
+            this.btnKategoriEkle.Margin = new System.Windows.Forms.Padding(6);
+            this.btnKategoriEkle.Name = "btnKategoriEkle";
+            this.btnKategoriEkle.Size = new System.Drawing.Size(242, 68);
+            this.btnKategoriEkle.TabIndex = 6;
+            this.btnKategoriEkle.Text = "KATEGORİ EKLE";
+            this.btnKategoriEkle.UseVisualStyleBackColor = false;
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnListele.Location = new System.Drawing.Point(1202, 264);
+            this.btnListele.Margin = new System.Windows.Forms.Padding(6);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(242, 68);
+            this.btnListele.TabIndex = 7;
+            this.btnListele.Text = "LİSTELE";
+            this.btnListele.UseVisualStyleBackColor = false;
+            // 
+            // dgvKategoriler
+            // 
+            this.dgvKategoriler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKategoriler.Location = new System.Drawing.Point(15, 15);
+            this.dgvKategoriler.Margin = new System.Windows.Forms.Padding(6);
+            this.dgvKategoriler.MultiSelect = false;
+            this.dgvKategoriler.Name = "dgvKategoriler";
+            this.dgvKategoriler.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvKategoriler.Size = new System.Drawing.Size(1174, 495);
+            this.dgvKategoriler.TabIndex = 5;
+            // 
+            // lblisim
+            // 
+            this.lblisim.AutoSize = true;
+            this.lblisim.Location = new System.Drawing.Point(1297, 15);
+            this.lblisim.Name = "lblisim";
+            this.lblisim.Size = new System.Drawing.Size(46, 24);
+            this.lblisim.TabIndex = 16;
+            this.lblisim.Text = "İSİM";
+            // 
+            // lblaciklama
+            // 
+            this.lblaciklama.AutoSize = true;
+            this.lblaciklama.Location = new System.Drawing.Point(1267, 97);
+            this.lblaciklama.Name = "lblaciklama";
+            this.lblaciklama.Size = new System.Drawing.Size(104, 24);
+            this.lblaciklama.TabIndex = 16;
+            this.lblaciklama.Text = "AÇIKLAMA";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1472, 620);
+            this.Controls.Add(this.lblaciklama);
+            this.Controls.Add(this.lblisim);
+            this.Controls.Add(this.btnYokEt);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnSilinenler);
+            this.Controls.Add(this.btnGuncellenenler);
+            this.Controls.Add(this.btnAktifler);
+            this.Controls.Add(this.txtAciklama);
+            this.Controls.Add(this.txtIsim);
+            this.Controls.Add(this.btnKategoriEkle);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.dgvKategoriler);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKategoriler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYokEt;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnSilinenler;
+        private System.Windows.Forms.Button btnGuncellenenler;
+        private System.Windows.Forms.Button btnAktifler;
+        private System.Windows.Forms.TextBox txtAciklama;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.Button btnKategoriEkle;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.DataGridView dgvKategoriler;
+        private System.Windows.Forms.Label lblisim;
+        private System.Windows.Forms.Label lblaciklama;
+    }
+}
